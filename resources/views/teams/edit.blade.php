c623d3f5e521c41f6d5bbb687a3b68b5df1601f2
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-09-25 22:56:03 +0200

    edit team css done

--- resources/views/teams/edit.blade.php
+++ resources/views/teams/edit.blade.php
@@ -1,29 +1,37 @@
-<h1>Edit Team</h1>
+<link rel="stylesheet" href="{{ asset('css/editForm.css') }}">
+<div class="form-container">
+    <h1>Edit Team</h1>
+    <form action="{{ route('teams.update', $team->id) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
 
-<form action="{{route('teams.update', $team->id)}}" method="POST" enctype="multipart/form-data">
-    @csrf
-    @method('PUT')
-
-    <label for="name">Name</label>
-    <input type="text" name='name' value="{{old('name', $team->name)}}"><br>
-
-    <label for="description">Description</label>
-    <textarea name="description" id="description">{{old('description', $team->description)}}</textarea><br>
-
-    <label for="city">City</label>
-    <input type="text" name='city' value="{{old('city', $team->city)}}"><br>
-
-    <label for="logo">Logo</label>
-    <input type="file" name='logo'>
-
-    @if($team->logo)
-        <div>
-            <p>Current Logo</p>
-            <img src="{{asset('storage/'. $team->logo)}}" alt="" width="150">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" name="name" value="{{ old('name', $team->name) }}">
         </div>
-    @endif
 
-    <button type="submit">Update</button>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea name="description" id="description">{{ old('description', $team->description) }}</textarea>
+        </div>
 
+        <div class="form-group">
+            <label for="city">City</label>
+            <input type="text" name="city" value="{{ old('city', $team->city) }}">
+        </div>
 
-</form>
+        <div class="form-group">
+            <label for="logo">Logo</label>
+            <input type="file" name="logo">
+        </div>
+
+        @if($team->logo)
+            <div class="current-logo">
+                <p>Current Logo</p>
+                <img src="{{ asset('storage/' . $team->logo) }}" alt="" width="100">
+            </div>
+        @endif
+
+        <button type="submit" class="btn-submit">Update</button>
+    </form>
+</div>
