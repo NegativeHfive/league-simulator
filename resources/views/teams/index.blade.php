0600d413303b2dad30826609953866cd220f2333
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-09-25 22:37:40 +0200

    teams can be updated, background color for index file in teams folder done. Color theme chnaged. Styling complete.

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -5,11 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team List</title>
     <link rel="stylesheet" href="{{ asset('css/teams.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/buttons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/a.css') }}">
 </head>
-<body style="text-align: center">
-    <div class="links" style="display: flex; width: ;">
+<body style="text-align: center;">
+    <div class="links" style="display: flex; width: 100%; justify-content: space-evenly; margin-top: 3%;">
+        <form action="{{ route('dashboard') }}" method="GET">
+            <button type="submit" class="btn">Home</button>
+        </form>
+
         <form action="{{ route('teams.index') }}" method="GET">
-            <button type="submit" class="btn">View Teams</button>
+            <button type="submit" class="btn">Teams</button>
         </form>
 
         <form action="{{ route('match.index') }}" method="GET">
@@ -25,9 +31,9 @@
         {{ session('success') }}
     </div>
     @endif
-    <h1>Here is the list of all the teams</h1>
+    <h1 style="color: white">All Teams</h1>
 
-    <div class="teams">
+    <div class="teams" >
         @foreach ($teams as $team)
             <div class="team-card">
                 <h2>{{ $team->name }}</h2>
@@ -35,8 +41,8 @@
                 <p>{{ $team->description }}</p>
                 <h4>{{ $team->city }}</h4>
                 <div class="aLinks">
-                    <a href="{{ route('teams.edit', $team->id) }}">Edit</a><br>
-                    <a href="{{ route('teams.delete', $team->id) }}">Delete</a>
+                    <a href="{{ route('teams.edit', $team->id) }}" style="background-color:purple; color: wheat;">Edit</a><br>
+                    <a href="{{ route('teams.delete', $team->id) }}" style="background-color: pink">Delete</a>
                 </div>
             </div>
         @endforeach
